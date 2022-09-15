4156693d49d2030bd8351dfe69da863ca8aa5591
Вячеслав Форофонов <DanielResult1@outlook.com>
2022-09-15 20:37:45 +0300

    Добавьте файлы проекта.

---
+++ Homework_31/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arrayNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Console.WriteLine("Исходный массив: ");
+
+        foreach (int numbers in arrayNumbers)
+        {
+            Console.Write($"{numbers} ");
+        }
+
+        Shuffle(arrayNumbers);
+
+        Console.WriteLine("\nМассив после перемешивания: ");
+
+        foreach (int numbers in arrayNumbers)
+        {
+            Console.Write($"{numbers} ");
+        }
+    }
+    static int[] Shuffle(int[] arrayNumbers)
+    {
+        int lastIndex = arrayNumbers.Count()-1;
+
+        while (lastIndex > 0)
+        {
+            int tempValue = arrayNumbers[lastIndex];
+            int randomIndex = new Random().Next(0, lastIndex);
+
+            arrayNumbers[lastIndex] = arrayNumbers[randomIndex];
+            arrayNumbers[randomIndex] = tempValue;
+            lastIndex--;
+        }
+        return arrayNumbers;
+    }
+}
